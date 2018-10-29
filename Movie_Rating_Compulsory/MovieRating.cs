a3a5651e8bcea47ba47212921214b1f1a7ad64cc
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-10-29 09:40:37 +0100

    NuGet and Review class

--- Movie_Rating_Compulsory/MovieRating.cs
+++ Movie_Rating_Compulsory/MovieRating.cs
@@ -1,8 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Movie_Rating_Compulsory
 {
     public class MovieRating : IMovieRating
     {
+        public double AvgOfReviewer(int rID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GradeCountByID(int rID, int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> MovieMostTopRate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double MovieReviewAvg(int mID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MovieReviewByGrade(int mID, int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MovieReviewCount(int mID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> MovieReviewers(int mID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumberOfReviews(int rID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> RevieverMovies(int rID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> ReviewerTopCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> TopMovies(int num)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Movie_Rating_Compulsory/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie_Rating_Compulsory
+{
+    public class Review
+    {
+        public int Reviewer { get; set; }
+        public int Movie { get; set; }
+        public int Grade { get; set; }
+        public string Date { get; set; }
+    }
+}
