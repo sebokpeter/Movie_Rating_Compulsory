6838ffc6832d2eef33a4f6a4f7155574d19d7a8b
Péter Sebők <sebok.peter97@gmail.com>
2018-10-29 10:44:28 +0100

    Added ReadJson method

--- MovieReviewTest/UnitTest1.cs
+++ MovieReviewTest/UnitTest1.cs
@@ -1,10 +1,29 @@
+using Movie_Rating_Compulsory;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using Xunit;
 
 namespace MovieReviewTest
 {
     public class UnitTest1
     {
+
+        List<Review> ReadJSON(string path)
+        {
+            List<Review> reviews = new List<Review>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                // TODO: Optional task: do not read everything at once.
+                string json = sr.ReadToEnd();
+                reviews = JsonConvert.DeserializeObject<List<Review>>(json);
+            }
+
+
+            return reviews;
+        }
         
     }
 }
