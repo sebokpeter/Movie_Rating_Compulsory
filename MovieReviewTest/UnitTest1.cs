661410c6451408899062fb38beafed611ba0f1bf
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-10-29 10:45:39 +0100

    commit

--- MovieReviewTest/UnitTest1.cs
+++ MovieReviewTest/UnitTest1.cs
@@ -1,10 +1,22 @@
+using Movie_Rating_Compulsory;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MovieReviewTest
 {
     public class UnitTest1
     {
-        
+        [Fact]
+        public void NumberOfReviews()
+        {
+            IMovieRating mr = new MovieRating();
+            List<Review> list = new List<Review>();
+
+            mr.Reviews = list;
+            int res = mr.NumberOfReviews(1);
+            int exp = 10;
+            Assert.Equal(res, exp);
+        }
     }
 }
